6ed64c107509044a2dac85d9115f789b822fc9d9
patysolano <patriciasolano383@gmail.com>
2022-05-19 02:48:52 -0500

    css y js - Home page
    hoja de estilo, burger navbar, codigo js para desplazamiento del navbar al bajar la página

---
+++ Vista/js/script.js
@@ -0,0 +1,15 @@
+let navbar = document.querySelector('.header .header-2 .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+
+   if(window.scrollY > 60){
+      document.querySelector('.header .header-2').classList.add('active');
+   }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+   }
+}
