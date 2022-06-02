b2425964e25f0b0b40e026b3c5e115ade855ce27
patysolano <patriciasolano383@gmail.com>
2022-06-01 20:41:01 -0500

    Header, Footer php

---
+++ Vista/footer.php
@@ -0,0 +1,30 @@
+<!-- Footer -->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <div class="logo">
+                <a href="#"><img src="img/logo.png"></a>
+            </div>
+            <p>
+                Misión
+                “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua”.
+            </p>
+        </div>
+
+        <div class="box">
+            <p><a href="#">INICIO</a></p>
+            <p><a href="#">PRODUCTOS</a></p>
+            <p><a href="#">PROMOCIONES</a></p>
+            <p><a href="#">CONTACTO</a></p>
+        </div>
+        <div class="box">
+            <h2>Redes sociales</h2>
+            <a href="#"><i class="fab fa-facebook-f"></i> Facebook </a>
+            <a href="#"><i class="fab fa-instagram"></i> Instagram </a>
+            <a href="#"><i class="fab fa-whatsapp"></i> Whatsapp </a>
+        </div>
+    </div>
+</section>
+
+<p class="credit"> &copy; <b>Delisweetchocolate</b> - 2022 Todos los Derechos Reservados</p>
---
+++ Vista/header.php
@@ -0,0 +1,35 @@
+<!-- Header -->
+<header class="header">
+    <div class="header-1">
+        <div class="flex">
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-whatsapp"></a>
+            </div>
+            <p> Nuevo <a href="login.html">Login</a> | <a href="registrarse.html">Registro</a> </p>
+        </div>
+    </div>
+
+    <div class="header-2">
+        <div class="flex">
+            <div class="logo">
+                <a href="#"><img src="img/logo.png"></a>
+            </div>
+            <!--BURGER NAVBAR INICIO-->
+            <nav class="navbar">
+                <a href="#">Catalogo</a>
+                <a href="#">Promociones</a>
+                <a href="#">Contacto</a>
+                <a href="#">Nosotros</a>
+            </nav>
+            <div class="icons">
+                <div id="menu-btn" class="fas fa-bars"></div>
+                <a href="#" class="fas fa-search"></a>
+                <div id="user-btn" class="fas fa-user"></div>
+                <a href="#"> <i class="fas fa-shopping-cart"></i></a>
+            </div>
+            <!--BURGER NAVBAR FIN-->
+        </div>
+    </div>
+</header>
